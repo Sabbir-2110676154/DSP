e38ee34c49095e1e9515c833d40c59bf99b110ca
Sabbir Ahmed <114326056+Sabbir-2110676154@users.noreply.github.com>
2025-05-14 23:10:51 +0600

    Update 1.py

--- dsp-main/1.py
+++ dsp-main/1.py
@@ -46,3 +46,88 @@ plt.ylabel("Amplitude")
 
 plt.tight_layout()
 plt.show()
+
+or,
+
+import numpy as np
+import matplotlib.pyplot as plt
+np.random.seed(42) #FIXED random value
+# Generate Unit Step Sequence
+n = np.arange(-10, 11, 1)  # Range from -10 to 10 increment 1
+u_n = np.where(n >= 0, 1, 0)  # Unit step: 1 if n >= 0, else 0
+print(n)
+print(u_n)
+
+#plt.plot(n,u_n)
+plt.stem(n,u_n,label="Unit Step Seq")
+plt.xlabel("n",color='red')
+plt.ylabel("u_n")
+plt.legend()
+plt.grid()
+
+plt.show()
+
+
+
+n=np.arange(-10,11,1)
+ramp=np.where(n<0,0,n)
+
+plt.scatter(n,ramp)
+plt.stem(n,ramp,label="Ramp Sequence")
+plt.xlabel("n")
+plt.ylabel("r(n)")
+plt.legend(loc='best')
+plt.grid(True)
+plt.show()
+
+
+
+# Parameters
+A = 1      # Amplitude
+b = 0.5    # Growth rate (decay for negative values)
+t = np.linspace(0, 10, 50)  # Time from 0 to 10, 500 points
+# Exponential function
+y = A * np.exp(b * t)
+
+# Plotting
+#plt.plot(t, y)
+plt.stem(t,y)
+plt.title('Exponential Growth')
+plt.xlabel('Time (t)')
+plt.ylabel('y(t)')
+plt.grid(True)
+plt.show()
+
+x=np.linspace(0,2*np.pi,1000) # 1000 points between 0 and 2π
+A=4
+
+y1=np.sin(x) #Sine Wave
+y2=np.cos(x) #Cos Wave
+
+plt.figure(figsize=(15,4)) #Figure Size
+
+#ax1=fig.add_subplot(1,2,1) #
+#ax1.plot(x,y1,color='green' , label='Sine wave')
+
+plt.subplot(2,2,1)
+plt.plot(x,y1,color='green' , label='Sine wave')
+plt.xlabel("Time")
+plt.ylabel("Amplitude")
+plt.legend(loc='best')
+plt.axhline(y=0,color='black') #horizontal line
+plt.axvline(x=0,color='red') #vertical line
+plt.title("Sine Wave")
+
+
+#ax2=fig.add_subplot(1,2,2)
+#ax2.plot(x,y2,color='green' , label="Cos Wave")
+plt.subplot(2,2,4)
+plt.plot(x,y2,color='green' , label="Cos Wave")
+plt.xlabel("Time")
+plt.ylabel("Amplitude")
+plt.legend(loc='best')
+plt.axhline(y=0,color='green') #horizontal line
+plt.axvline(x=0,color='red') #vertical line
+plt.title("Cos Wave")
+
+plt.show() #Render Plot
